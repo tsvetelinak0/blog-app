7e604befbf63626f507b3474bbd93d33889f3165
Tsvetelina Koleva <tsekoleva@gmail.com>
2021-02-18 20:57:53 +0000

    added django-heroku

--- try_django/settings.py
+++ try_django/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -132,3 +133,5 @@ STATICFILES = [
 
 MEDIA_ROOT = os.path.join(LOCAL_STATIC_CDN_PATH, 'media')
 MEDIA_URL = '/media/'
+
+django_heroku.settings(locals())
